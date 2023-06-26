70aea1dd8eb1243579bef92b9ecd7b19a3c3c113
aws1313 <69417093+aws1313@users.noreply.github.com>
2023-06-26 10:58:26 +0200

    ready for testing

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import requests
 import json
+import platformdirs
+import os
+import datetime
+import operator
 
-with open("creds.json") as f:
-    CREDS = json.loads(f.read())
+APPNAME = "ben_skyblock_collection_stream"
+AUTHOR = "io.github.aws1313"
+CACHE_DIR = platformdirs.user_cache_dir(APPNAME, AUTHOR)
+DATA_DIR = platformdirs.user_data_dir(APPNAME, AUTHOR)
 
 
-def get_profile_collection(api_key, uuid, profile_id):
+def get_profile_collection(api_key, profile_id, uuid):
     url = f"https://api.hypixel.net/skyblock/profile?key={api_key}&profile={profile_id}&uuid={uuid}"
+    response = requests.get(url)
+    return response.json()
+
+
+def prepare_dirs():
+    os.makedirs(DATA_DIR, exist_ok=True)
+    os.makedirs(CACHE_DIR, exist_ok=True)
+    print(f"{CACHE_DIR=}; {DATA_DIR=}")
+
+
+def get_collected_items():
+    url = f"https://api.hypixel.net/resources/skyblock/collections"
     response = requests.get(url)
     return response.json()
 
@@ -16,40 +34,124 @@ def save_to_json(filename, data):
         json.dump(data, file, indent=4)
 
 
-# Set your Hypixel API key here
-api_key = CREDS["api_key"]
+def read_from_json(filename):
+    with open(filename) as f:
+        return json.loads(f.read())
 
-# Set the UUID for which you want to track collections
-uuid = CREDS["uuid"]
-profile_id = CREDS["profile_id"]
 
-# Get the collection data from the API
-response_json = get_profile_collection(api_key, uuid, profile_id)
-print(response_json)
-with open("test.json","w") as f:
-    f.write(json.dumps(response_json))
-
-# Check if profile and members data exists in the response
-if 'profile' in response_json and 'members' in response_json['profile']:
-    members = response_json['profile']['members']
-
-    # Check if the UUID exists in the members data
-    if uuid in members:
-        profile_data = members[uuid]
-
-        # Check if unlocked_coll_tiers exists and is a list
-        if 'unlocked_coll_tiers' in profile_data and isinstance(profile_data['unlocked_coll_tiers'], list):
-            unlocked_coll_tiers = profile_data['unlocked_coll_tiers']
-
-            # Sort the unlocked collection tiers
-            sorted_coll_tiers = sorted(unlocked_coll_tiers)
-
-            # Save the sorted collection tiers to a JSON file
-            #save_to_json('unlocked_coll_tiers.json', sorted_coll_tiers)
-            print("Unlocked collection tiers saved and sorted to 'unlocked_coll_tiers.json' file.")
-        else:
-            print("No unlocked collection tiers found for the UUID.")
+def load_conf():
+    conf_path = os.path.join(DATA_DIR, "config.json")
+    if os.path.exists(conf_path):
+        with open(conf_path) as f:
+            return json.loads(f.read())
     else:
-        print("UUID not found in profile members.")
-else:
-    print("Invalid response or profile data not found.")
+        save_to_json(conf_path, {"api-key": "", "uuid": "", "profile-id": ""})
+        exit(f"Config did not exist please fill out: {conf_path}")
+
+
+def prepare_collection_infos():
+    collection_info_path = os.path.join(CACHE_DIR, "collection_info.json")
+    if os.path.exists(collection_info_path) and (datetime.datetime.fromtimestamp(
+            os.path.getmtime(collection_info_path)) > datetime.datetime.now() - datetime.timedelta(days=1)):
+        return read_from_json(collection_info_path)
+    else:
+        url = "https://api.hypixel.net/resources/skyblock/collections"
+        response = requests.get(url)
+        print("Got new collection_infos")
+        save_to_json(collection_info_path, response.json())
+        return response.json()
+
+
+def evaluate_changes(old: dict, new: dict, old_collections):
+    collected = new["profile"]["members"][uuid]["collection"]
+    collected_old = old["profile"]["members"][uuid]["collection"]
+    collections = old_collections
+    for k in collected.keys():
+        collections[k] = {"collected": collected[k]}
+        if k in collected_old.keys() and (collected[k] != collected_old[k]):
+            collections[k]["last_changed"] = datetime.datetime.now()
+            collections[k]["changed"] = True
+            collections[k]["amount_changed"] = collected[k] - collected_old[k]
+        else:
+            collections[k]["changed"] = False
+            collections[k]["amount_changed"] = 0
+        if "last_changed" not in collections[k]:
+            collections[k]["last_changed"] = 0
+
+    return collections
+
+
+def get_collections(old: dict, new: dict, collection_infos, old_collections):
+    # Target [{"id":"COLLECTION", "display_name":"DISPLAY_NAME", "collected":1000, "tier_now":0,"missing_to_next_tier":500, "percentage_to_next_tier":0.75, "last_changed":0}]
+    coll = evaluate_changes(old, new, old_collections)
+    print(coll)
+
+    items = {}
+    for i in collection_infos["collections"].keys():
+        for h in collection_infos["collections"][i]["items"].keys():
+            items[h] = collection_infos["collections"][i]["items"][h]
+
+    print(items)
+    for c in coll.keys():
+        coll[c]["display_name"] = items[c]["name"]
+        coll[c]["id"] = c
+        tier = 0
+        # get tier now
+        for tt in range(len(items[c]["tiers"])):
+            t = items[c]["tiers"][tt]
+            if coll[c]["collected"] > t["amountRequired"]:
+                tier = t["tier"]
+                if tier == items[c]["maxTiers"]:
+                    coll[c]["maxed_out"] = True
+                    coll[c]["missing_to_next_tier"] = 0
+                    coll[c]["percentage_to_next_tier"] = 1
+                else:
+                    coll[c]["maxed_out"] = False
+                    next_needed = items[c]["tiers"][tt + 1]["amountRequired"]
+                    coll[c]["missing_to_next_tier"] = next_needed - coll[c]["collected"]
+                    coll[c]["percentage_to_next_tier"] = coll[c]["missing_to_next_tier"] / (
+                            next_needed - t["amountRequired"])
+
+            else:
+                break
+        coll[c]["tier_now"] = tier
+
+    return coll
+
+
+if __name__ == '__main__':
+    prepare_dirs()
+
+    # CONFIG
+    conf = load_conf()
+    api_key = conf["api-key"]
+    uuid = conf["uuid"]
+    profile_id = conf["profile-id"]
+
+    collection_infos = prepare_collection_infos()
+
+    # repeat
+    profile_collections_old = get_profile_collection(api_key, profile_id, uuid)
+    old_collection = {}
+    while True:
+        inp = input("Press enter to continue")
+        if inp == "e":
+            exit()
+
+        profile_collections_new = get_profile_collection(api_key, profile_id, uuid)
+        c = get_collections(profile_collections_old, profile_collections_new, collection_infos, old_collection)
+        print(c)
+        sort = list(c.values())
+        sort.sort(key=operator.itemgetter('last_changed'))
+
+        print(sort)
+
+        changed = []
+        for s in sort:
+            if s["changed"]:
+                changed.append(s)
+
+        print(changed)
+
+        profile_collections_old = profile_collections_new
+        old_collection = c
