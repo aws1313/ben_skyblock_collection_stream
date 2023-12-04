72374d8d46a0ada3b0b2a86142e394ac3244e161
aws1313 <69417093+aws1313@users.noreply.github.com>
2023-12-04 19:58:33 +0100

    dockerize

--- constants.py
+++ constants.py
@@ -1,11 +1,10 @@
-import platformdirs
 import json
 from PIL import Image
 
 APPNAME = "ben_skyblock_collection_stream"
 AUTHOR = "io.github.aws1313"
-CACHE_DIR = platformdirs.user_cache_dir(APPNAME, AUTHOR)
-DATA_DIR = platformdirs.user_data_dir(APPNAME, AUTHOR)
+CACHE_DIR = "/data/cache"
+DATA_DIR = "/data/data"
 default_img = Image.new("RGBA", (750,150))
 
 
